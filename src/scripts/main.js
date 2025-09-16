7f7ffb8f5a921f50eb972f7598bc5428a57deee3
Dmytro Shovheniuk <dimashovgenyuk06@gmail.com>
2025-09-15 21:37:23 -0500

    wrote first code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,80 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
 // const Game = require('../modules/Game.class');
-// const game = new Game();
 
-// Write your code here
+class Game {
+  constructor() {
+    this.matrix = [
+      [0, 2, 0, 2],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    this.score = 0;
+  }
+
+  moveLeft() {
+    console.log('hi');
+    function shiftRowLeft(matrix) {
+      for (let i of matrix) {
+        for (let j of i) {
+          if (j ===)
+        }
+      }
+    }
+  }
+
+  moveRight() {
+    console.log('hi');
+  }
+
+  moveUp() {
+    console.log('hi');
+  }
+
+  moveDown() {
+    console.log('hi');
+  }
+
+  // getState() {
+
+  // }
+
+  // getScore() {
+
+  // }
+
+  // getStatus() {
+
+  // }
+
+  // start() {
+
+  // }
+
+  // restart() {
+
+  // }
+}
+
+const game = new Game();
+
+
+// document.addEventListener('keydown', event => console.log(event.key));
+document.addEventListener('keydown', event => {
+  switch (event.key) {
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+  }
+});

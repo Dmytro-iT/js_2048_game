c3290afd43621575ca76b18096a5313918178011
Dmytro Shovheniuk <dimashovgenyuk06@gmail.com>
2025-09-17 20:29:33 -0500

    2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,36 +5,159 @@
 class Game {
   constructor() {
     this.matrix = [
-      [0, 2, 0, 2],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0]
+      [2, 4, 0, 0],
+      [0, 4, 0, 2],
+      [2, 0, 2, 0],
+      [0, 2, 0, 4]
     ];
 
     this.score = 0;
   }
 
-  moveLeft() {
-    console.log('hi');
-    function shiftRowLeft(matrix) {
-      for (let i of matrix) {
-        for (let j of i) {
-          if (j ===)
+  shiftRowLeft(row) {
+    let newRow = row.filter(num => num !== 0);
+
+    for (let i = 0; i < newRow.length - 1; i++) {
+      if (newRow[i] === newRow[i + 1]) {
+        newRow[i] *= 2;
+        newRow[i + 1] = 0;
+        i++;
+      }
+    }
+
+    newRow = newRow.filter(num => num !== 0);
+
+    while (newRow.length < 4) {
+      newRow.push(0);
+    }
+
+    return newRow;
+  }
+
+  shiftRowRight(row) {
+    let newRow = row.filter(num => num !== 0);
+
+    for (let i = newRow.length - 1; i > 0; i--) {
+      if (newRow[i] === newRow[i - 1]) {
+        newRow[i] *= 2;
+        newRow[i - 1] = 0;
+        i--;
+      }
+    }
+
+    newRow = newRow.filter(num => num !== 0);
+
+    while(newRow.length < 4) {
+      newRow.unshift(0);
+    }
+
+    return newRow;
+  }
+
+  shiftRowUp(matrix) {
+    let newRow = [];
+    let newMatrix = matrix;
+    let resultMatrix = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        newRow.push(newMatrix[j][i]);
+
+        if (newRow.length === 4) {
+          newRow = newRow.filter(num => num !== 0);
+          console.log(newRow);
+
+          for (let n = 0; n < newRow.length - 1; n++) {
+            if (newRow[n] === newRow[n + 1]) {
+              newRow[n] *= 2;
+              newRow[n + 1] = 0;
+              n++;
+            }
+          }
+
+          newRow = newRow.filter(num => num !== 0);
+
+          while (newRow.length < 4) {
+            newRow.push(0);
+          }
+
+          for (let z = 0; z < 4; z++) {
+            resultMatrix[z][i] = newRow[z];
+          }
+
+          newRow = [];
         }
       }
     }
+    return resultMatrix;
+  }
+
+  shiftRowDown(matrix) {
+    let newRow = [];
+    let newMatrix = matrix;
+    let resultMatrix = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        newRow.push(newMatrix[j][i]);
+
+        if (newRow.length === 4) {
+          newRow = newRow.filter(num => num !== 0);
+          console.log(newRow);
+
+          for (let n = newRow.length - 1; n > 0; n--) {
+            if (newRow[n] === newRow[n - 1]) {
+              newRow[n] *= 2;
+              newRow[n - 1] = 0;
+              n--;
+            }
+          }
+
+          newRow = newRow.filter(num => num !== 0);
+
+          while (newRow.length < 4) {
+            newRow.unshift(0);
+          }
+
+          for (let z = 0; z < 4; z++) {
+            resultMatrix[z][i] = newRow[z];
+          }
+
+          newRow = [];
+        }
+      }
+    }
+    return resultMatrix;
+  }
+
+  moveLeft() {
+    this.matrix = this.matrix.map(row => this.shiftRowLeft(row));
+    console.log(this.matrix);
   }
 
   moveRight() {
-    console.log('hi');
+    this.matrix = this.matrix.map(row => this.shiftRowRight(row));
+    console.log(this.matrix);
   }
 
   moveUp() {
-    console.log('hi');
+    this.matrix = this.shiftRowUp(this.matrix);
+    console.log(this.matrix);
   }
 
   moveDown() {
-    console.log('hi');
+    this.matrix = this.shiftRowDown(this.matrix);
+    console.log(this.matrix);
   }
 
   // getState() {
